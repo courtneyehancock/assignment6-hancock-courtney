ba5af26adff7a1c58db992ce1c42f440be956639
Courtney Hancock <ceh.courtney@gmail.com>
2018-12-01 20:42:32 -0800

    added an object, array and while loop

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+//Array band member names
+var members = [
+    'Dave Grohl',
+    'Nate Mendel',
+    'Pat Smear',
+    'Taylor Hawkins',
+    'Rami Jaffee',
+    'Chris Shiflett'
+];
+
+//Object tour cities and dates
+var tour = {
+    Chicago: "July 29th & 30th, 2018",
+    Seattle: "September 1st, 2018",
+    Edmonton: "September 4th, 2018"
+};
+
+//while loop
+var i = 0;
+var x =
+while ( i < 5 ) {
+  console.log ( 'Times running through the loop i');
+
+  i++;
+}
